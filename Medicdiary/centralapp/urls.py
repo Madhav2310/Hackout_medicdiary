7808a53679f2e1e6e9d9ae5e0d9c93c9ac5ac9b6
Lavish Bansal <lavish.bansal.mat19@iitbhu.ac.in>
2020-11-07 14:00:31 +0530

    urls, views added and how to use html pages connected

--- Medicdiary/centralapp/urls.py
+++ Medicdiary/centralapp/urls.py
@@ -5,9 +5,15 @@ app_name = 'centralapp'
 urlpatterns = [
 
     path('',views.mainpage,name='mainpage'),
+    path('Aids/',views.Aids,name ='Aids'),
+    path('Cancer/',views.Cancer,name ='Cancer'),
+    path('Covid-19/',views.Covid_19,name ='Covid_19'),
+    path('Diabetes/',views.Diabetes,name ='Diabetes'),
     path('FAQS/',views.FAQS,name ='FAQS'),
+    path('Heart_disorder/',views.Heart_disorder,name ='Heart_disorder'),
     path('medical_practitioners/How_to_use',views.doc_how_to_use,name ='doc_how_to_use'),
     path('friends-and-family/How_to_use',views.patients_how_to_use,name ='patients_how_to_use'),
+    path('Hypertension/',views.Hypertension,name ='Hypertension'),
     path('Inside_health_records/',views.Inside_health_records,name ='Inside_health_records'),
     path('login/',views.login,name='login'),
 ]
