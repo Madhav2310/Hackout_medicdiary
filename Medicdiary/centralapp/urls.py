00c5bdae1afecc904d0af25db3aad69507adb015
Madhav Mittal <59472922+Madhav2310@users.noreply.github.com>
2020-11-07 07:54:50 +0530

    Update urls.py

--- Medicdiary/centralapp/urls.py
+++ Medicdiary/centralapp/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
 
     path('',views.mainpage,name='mainpage'),
     path('About_us/',views.About_us,name ='About_us'),
+    path('FAQS/',views.FAQS,name ='FAQS'),
     path('medical_practitioners/How_to_use',views.doc_how_to_use,name ='doc_how_to_use'),
     path('friends-and-family/How_to_use',views.patients_how_to_use,name ='patients_how_to_use'),
     path('Inside_health_records/',views.Inside_health_records,name ='Inside_health_records'),
